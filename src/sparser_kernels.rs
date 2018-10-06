aae93f8a4c2c585197c76bcb783ef536b7d2f84a
Wangweizhen <hawking.rei@gmail.com>
2018-10-06 12:50:26 +0800

    update

--- src/sparser_kernels.rs
+++ src/sparser_kernels.rs
@@ -2,6 +2,36 @@
 use std::arch::x86::*;
 #[cfg(target_arch = "x86_64")]
 use std::arch::x86_64::*;
+
+fn ffs(x: i32) -> i32 {
+    let mut r: i32 = 1;
+    let mut val = x;
+    if (val == 0) {
+        return val;
+    }
+    if ((val & 0xffff) == 0) {
+        val >>= 16;
+        r += 16;
+    }
+    if ((val & 0xff) == 0) {
+        val >>= 8;
+        r += 8;
+    }
+    if ((val & 0xf) == 0) {
+        val >>= 4;
+        r += 4;
+    }
+    if ((val & 3) == 0) {
+        val >>= 2;
+        r += 2;
+    }
+    if ((val & 1) == 0) {
+        val >>= 1;
+        r += 1;
+    }
+    return r;
+}
+
 /** Search for an 8-bit search string.
  *
  * @param reg the register filled with the search value
@@ -13,9 +43,11 @@ pub fn search_epi8(reg: __m256i, base: Vec<char>) -> u32 {
     let mut count = 0;
     unsafe {
         let val = _mm256_loadu_si256(base.as_ptr() as *const __m256i);
-        let mask = _mm256_movemask_epi8(_mm256_cmpeq_epi8(reg, val));
+        let mut mask = _mm256_movemask_epi8(_mm256_cmpeq_epi8(reg, val));
         while (mask != 0) {
-            break;
+            let index = ffs(mask) - 1;
+            mask &= !(1 << index);
+            count = count + 1;
         }
     }
     return 0;
