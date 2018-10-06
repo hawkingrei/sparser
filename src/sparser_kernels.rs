dbe453b007936a0a19e20409871560af97316b86
Wangweizhen <hawking.rei@gmail.com>
2018-10-06 23:38:56 +0800

    update

--- src/sparser_kernels.rs
+++ src/sparser_kernels.rs
@@ -91,7 +91,9 @@ pub fn search_epi32(reg: __m256i, base: Vec<char>) -> u32 {
     unsafe {
         let val = _mm256_loadu_si256(base.as_ptr() as *const __m256i);
         let mut mask = _mm256_movemask_epi8(_mm256_cmpeq_epi32(reg, val));
+        println!("mask: {:?}", mask);
         mask &= 0x11111111;
+        println!("mask: {:?}", mask);
         while (mask != 0) {
             let index = ffs(mask) - 1;
             mask &= !(1 << index);
@@ -99,4 +101,23 @@ pub fn search_epi32(reg: __m256i, base: Vec<char>) -> u32 {
         }
     }
     return count;
+}
+
+#[cfg(test)]
+mod test {
+    use sparser_kernels::search_epi32;
+    use std::arch::x86_64::__m256i;
+    use std::arch::x86_64::_mm256_loadu_si256;
+
+    #[test]
+    fn test_search_epi32() {
+        unsafe {
+            let s: &str = "Asked whether an agreement could be reached at the next meeting of European leaders on 17 October. He said a deal could be agree";
+            let req: __m256i = _mm256_loadu_si256(s.as_ptr() as *const __m256i);
+            let base: Vec<char> = vec!['a', 'n'];
+            let result = search_epi32(req, base);
+            println!("{:?}", result);
+            assert!(result >= 0);
+        }
+    }
 }
